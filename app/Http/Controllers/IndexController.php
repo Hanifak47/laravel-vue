8ea07f228019eb4dc625c394c15567094e64a8f9
Hanifak47 <hanifsmurf@gmail.com>
2025-05-07 17:02:15 +0700

    linking page to page

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    //
+    public function index()
+    {
+        // return inertia(
+        //     'Index/Index',
+        //     [
+        //         'message' => 'Hello from laravel!'
+        //     ]
+        // );
+
+        return inertia('Index/Index');
+
+    }
+
+    public function show()
+    {
+        return inertia('Index/Show');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return inertia('Index/Index');
-});
+// Route::get('/', function () {
+//     return inertia('Index/Index');
+// });
+
+Route::get('/', [App\Http\Controllers\IndexController::class, 'Index']);
+Route::get('/hello', [App\Http\Controllers\IndexController::class, 'Show']);
