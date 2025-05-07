ffa4acfe156fe5067fdbde1cf1525c01515c4988
Hanifak47 <hanifsmurf@gmail.com>
2025-05-07 11:25:06 +0700

    configuting laravel, inertia, and vue

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    Hello {{ counter }}!
+</template>
+
+<script setup>
+    import {
+        ref
+    } from 'vue'
+    // console.log('Index.vue loaded')
+    // console.log('Index.vue loaded') 
+    const counter = ref(0)
+    setInterval(() => counter.value++, 1000)
+
+    // console.log('app.js loaded')
+
+</script>
