810bacb9a69bb9e3ecf5decda5003e3fbbe56929
Hanifak47 <hanifsmurf@gmail.com>
2025-05-22 21:14:29 +0700

    +integrasi tailwind css +styling form edit dan create +styling error message +penggunaan applied styling

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,33 +1,43 @@
 <template>
-  <Link href="/listing">Main Page</Link> &nbsp;
-  <Link href="/listing/create">Show Page</Link>
-
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
-  <!-- <div>{{ y }}</div> -->
-  <slot default></slot>
+  <header
+    class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full"
+  >
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link :href="route('listing.index')">Listings</Link>
+        </div>
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <Link :href="route('listing.index')">Listing App</Link>
+        </div>
+        <div>
+          <Link
+            :href="route('listing.create')"
+            class="btn-primary"
+            >+ New Listing</Link
+          >
+        </div>
+      </nav>
+    </div>
+  </header>
+  <main class="container mx-auto p-4">
+    <div
+      v-if="flashSuccess"
+      class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2"
+    >
+      {{ flashSuccess }}
+    </div>
+    <slot>Default</slot>
+  </main>
 </template>
 
 <script setup>
-// computed =menggunakan flash massage
 import { ref, computed } from "vue";
 import { Link, usePage } from "@inertiajs/vue3";
-// import { ref } from "vue";
 
 const x = ref(0);
 const y = computed(() => x.value * 2);
 
 const page = usePage();
 const flashSuccess = computed(() => page.props.Flash.success);
-
-// const timer = ref(0);
-// setInterval(() => timer.value++, 1000);
 </script>
-
-<style scoped>
-.success {
-  background-color: green;
-  color: white;
-}
-</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,12 +8,15 @@
     <title>Laravel</title>
 
     @routes
+    @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
-@inertia
 
-<body>
+{{-- keadaan normal theme dan dark theme --}}
+
+<body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300">
+    @inertia
 </body>
 
 </html>
